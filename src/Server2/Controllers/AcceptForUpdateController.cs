06ea1daa5289d01cbb2ca4dffa66ff9ea401c7dc
Tommy Sørbråten <tommysor@gmail.com>
2023-01-18 21:49:52 +0000

    ServiceBusReceivedMessage

--- src/Server2/Controllers/AcceptForUpdateController.cs
+++ src/Server2/Controllers/AcceptForUpdateController.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Azure.Messaging.ServiceBus;
 using Dapr;
 using Microsoft.AspNetCore.Mvc;
@@ -24,9 +25,14 @@ public class AcceptForUpdateController : Controller
 
     [HttpPost("AcceptForUpdate")]
     [Topic("servicebus-pub-sub", "send-update-request")]
-    public async Task<IActionResult> AcceptForUpdate([FromBody] ProcessMessageEventArgs item)
+    public async Task<IActionResult> AcceptForUpdate([FromBody] ServiceBusReceivedMessage item)
     {
         _logger.LogInformation("AcceptForUpdate: {item}", item);
+
+        var x1 = item.Body.ToArray();
+        var x2 = Encoding.UTF8.GetString(x1);
+        _logger.LogInformation("AcceptForUpdate Body: {x2}", x2);
+
         await Task.CompletedTask;
         return Accepted();
     }
