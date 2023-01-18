a2e8f8df2126130d2985c4bc810ab058bc7b9fa8
Tommy Sørbråten <tommysor@gmail.com>
2023-01-18 22:18:23 +0000

    dynamic and body from httpContext

--- src/Server2/Controllers/AcceptForUpdateController.cs
+++ src/Server2/Controllers/AcceptForUpdateController.cs
@@ -1,3 +1,4 @@
+using System.Buffers;
 using System.Text;
 using Azure.Messaging.ServiceBus;
 using Dapr;
@@ -11,10 +12,12 @@ namespace Server2.Controllers;
 public class AcceptForUpdateController : Controller
 {
     private readonly ILogger<AcceptForUpdateController> _logger;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public AcceptForUpdateController(ILogger<AcceptForUpdateController> logger)
+    public AcceptForUpdateController(ILogger<AcceptForUpdateController> logger, IHttpContextAccessor httpContextAccessor)
     {
         _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
     }
 
     [HttpGet]
@@ -25,13 +28,19 @@ public class AcceptForUpdateController : Controller
 
     [HttpPost("AcceptForUpdate")]
     [Topic("servicebus-pub-sub", "send-update-request")]
-    public async Task<IActionResult> AcceptForUpdate([FromBody] ServiceBusReceivedMessage item)
+    public async Task<IActionResult> AcceptForUpdate([FromBody] dynamic item)
     {
-        _logger.LogInformation("AcceptForUpdate: {item}", item);
-
-        var x1 = item.Body.ToArray();
-        var x2 = Encoding.UTF8.GetString(x1);
+        var context = _httpContextAccessor.HttpContext;
+        var x = await context.Request.BodyReader.ReadAsync();
+        var bytes = x.Buffer.ToArray();
+        var x2 = Encoding.UTF8.GetString(bytes);
         _logger.LogInformation("AcceptForUpdate Body: {x2}", x2);
+
+        // _logger.LogInformation("AcceptForUpdate: {item}", item);
+
+        // var x1 = item.Body.ToArray();
+        // var x2 = Encoding.UTF8.GetString(x1);
+        // _logger.LogInformation("AcceptForUpdate Body: {x2}", x2);
 
         await Task.CompletedTask;
         return Accepted();
